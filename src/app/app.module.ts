3a45f618b42c724c8e098fbc4f3226e133715d8b
Orleandro Lincoln <76212175+Vornak@users.noreply.github.com>
2023-10-29 12:17:35 -0300

    primeiro-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+
+];
+
 
 @NgModule({
   declarations: [
